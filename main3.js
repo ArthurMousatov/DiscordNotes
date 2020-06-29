73938654f83928d742aa773632c0aa6f4f0f1361
Venoshulk <arthurmousatov@gmail.com>
2020-06-29 17:42:33 -0400

    Added basic kicking/muting host privileges

--- main3.js
+++ main3.js
@@ -10,14 +10,17 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
+
 /*
 Array of Room objects
 Each object contains:
 
 -   room: room's name
+-   host: host's id
 -   users: list of sockets
 -   canvas: array of objects containing x and y coordinates
 */
+
 drawRooms = [];
 
 server.listen(port, () =>{
@@ -25,18 +28,20 @@ server.listen(port, () =>{
 });
 
 //string randomizer via https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-function makeid(length) {
+function MakeId(length) {
+
+    //Loop until the result is unique
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
 
 //Search for specific value in key via https://stackoverflow.com/questions/12462318/find-a-value-in-an-array-of-objects-in-javascript 
-function roomSearch(roomName, myArray){
+function RoomSearch(roomName, myArray){
     for (var i=0; i < myArray.length; i++) {
         if (myArray[i]['room'] === roomName) {
             return i;
@@ -45,6 +50,38 @@ function roomSearch(roomName, myArray){
     return null;
 }
 
+function GetSocket(usr, users){
+    for(let i = 0; i < users.length; i++){
+        if(users[i].username === usr){
+            return users[i];
+        }
+    }
+    return null;
+}
+
+//Get a list of usernames and if they are a host from a users array
+function GetNames(users){
+    let names = [];
+    let currentUser = {};
+    for(let i = 0; i < users.length; i++){
+        currentUser = {
+            name: users[i].username,
+            isHost: users[i].isHost
+        }
+        names.push(currentUser);
+    }
+    return names;
+}
+
+//Check if the name is already taken
+function isNameFree(name, users){
+    for(let i = 0; i < users.length; i++){
+        if(name === users[i].username){
+            return false;
+        }
+    }
+    return true;
+}
 
 //create app/json parser
 let jsonParser = bodyParser.json();
@@ -61,19 +98,24 @@ app.get('/', function(req, res){
 });
 
 app.get('/board', function(req, res){
-    let roomString = makeid(15);
+    let roomString = MakeId(15);
+    let hostID = MakeId(15);
+
     //Creation of new room
     let roomObj = {
         room: roomString,
+        host: hostID,
         users: [],
         canvas: []
     };
     drawRooms.push(roomObj);
+
     //Creation of user payload
     let data = {
-        roomCode: roomString
+        roomCode: roomString,
+        hostCode: hostID
     };
-    console.log("Created room " + roomString);
+    console.log("Created room " + roomString + " with host ID " + hostID);
     res.render('board', {data: data});
 });
 
@@ -85,21 +127,31 @@ app.get('/board/:id', function(req, res){
     res.render('board', {data: data});
 });
 
-let botId = "12354335asdfdserw32"
+let botId = "12354335asdfdserw32";
+
 //Allows the creation of a board if the id is approved
 app.get('/api/create/:id', function(req, res){
     if(req.params.id === botId){
-        let roomString = makeid(15);
+        let roomString;
+        let hostID = MakeId(15);
+
+        do{
+            roomString = MakeId(15);
+        }while(RoomSearch(roomString, drawRooms) !== null)
+
         //Creation of new room
         let roomObj = {
             room: roomString,
+            host: hostID,
             users: [],
             canvas: []
         };
         drawRooms.push(roomObj);
-        console.log("Create room " + roomString)
+        console.log("Create room " + roomString);
+
+        //Send JSON
         res.setHeader('Content-Type', 'application/json');
-        res.json({status: "OK", roomCode: roomString});
+        res.json({status: "OK", roomCode: roomString, hostCode: hostID});
     }else{
         res.setHeader('Content-Type', 'application/json');
         res.json({status: "BAD_ID"});
@@ -114,18 +166,67 @@ app.post('/board', urlEncodedParser, function(req, res){
 io.on("connection", (socket) =>{
 
     //Connect socket to a room
-    socket.on("joinRoom", (room) =>{
+    socket.on("joinRoom", (data) =>{
+
         //Check if room exists
-        let currentRoomIndex = roomSearch(room, drawRooms);
+        let currentRoomIndex = RoomSearch(data.room, drawRooms);
         if(currentRoomIndex != null){
-            socket.room = room;
+            socket.room = data.room;
+            socket.isMuted = false;
+
+            //Check if name is not unique
+            if(isNameFree(data.user, drawRooms[currentRoomIndex]['users'])){
+                console.log("Name is free");
+                socket.username = data.user;
+            }else{
+                let counter = 0;
+                let name = data.user + String(counter);
+                console.log(name);
+                //While name is not unique
+                while(!isNameFree(name, drawRooms[currentRoomIndex]['users'])){
+                    counter++;
+                    name = data.user + String(counter);
+                }
+                socket.username = name;
+            }
             socket.index = currentRoomIndex;
-            socket.join(room);
+            
+            //Get the room's current users
+            let users = GetNames(drawRooms[currentRoomIndex]['users']);
+
+            //Check if user has matching host id
+            if(data.host !== undefined){
+                if(drawRooms[currentRoomIndex]['host'] === data.host){
+                    socket.isHost = true;
+                }else{
+                    return socket.emit("err", "Error, wrong host ID: " + data.host);
+                }
+            }else{
+                socket.isHost = false;
+            }
+
+            socket.join(data.room);
             drawRooms[currentRoomIndex]['users'].push(socket);
 
-            return socket.emit("suc", drawRooms[socket.index]['canvas']);
+            //Send the new user info to other sockets
+            let sendData = {
+                user: socket.username,
+                host: socket.isHost,
+            };
+
+            socket.to(socket.room).emit("usrJoin", sendData);
+
+            //Send the room's current users + canvas to the new user
+            sendData = {
+                canvas: drawRooms[socket.index]['canvas'],
+                users: users,
+                isHost: socket.isHost
+            };
+            console.log(socket.isHost);
+
+            return socket.emit("suc", sendData);
         }else{
-            return socket.emit("err", "Error, No Room named " + room);
+            return socket.emit("err", "Error, No Room with code: " + data.room);
         }
     });
 
@@ -134,35 +235,79 @@ io.on("connection", (socket) =>{
         Data payload:
         -x: x representing the horizontal coordinate of the pixel
         -y: y representing the vertical coordinate of the pixel
-        -lastx: 
-        -lasty: 
+        -lastx: x representing the start of the draw
+        -lasty: y representing the start of the draw
         -size: size of the pixel
-        -color: color representing the color of the pixel
+        -color: color of the pixel
     */
+
     socket.on("draw", function(data){
-        socket.to(socket.room).emit("draw", data);
-        //save the pixel data into canvas array
-        drawRooms[socket.index]['canvas'].push({x: data.x, y: data.y, lastx: data.lastx, lasty: data.lasty, size: data.size, color: data.color});
+        if(!socket.isMuted){ 
+            socket.to(socket.room).emit("draw", data);
+
+            //save the pixel data into canvas array
+            drawRooms[socket.index]['canvas'].push({x: data.x, y: data.y, lastx: data.lastx, lasty: data.lasty, size: data.size, color: data.color});
+        }
+    });
+
+    socket.on("usrKick", (data) => {
+        //If socket is a host
+        if(socket.isHost){
+            console.log("Kicking " + data.user);
+            let kickedSocket = GetSocket(data.user, drawRooms[socket.index]['users']);
+            
+            if(kickedSocket !== null){
+                kickedSocket.emit('kicked');
+                kickedSocket.leave(socket.room);
+
+                // let data = {
+                //     user: kickedSocket.username
+                // };
+                // kickedSocket.to(kickedSocket.room).emit("usrLeft", data);
+
+                // let kickedSocketIndex = drawRooms[kickedSocket.index]['users'].indexOf(kickedSocket);
+                // drawRooms[kickedSocket.index]['users'].splice(kickedSocketIndex, 1);
+            }
+        }
+    });
+
+    socket.on("usrMute", (data) => {
+        if(socket.isHost){
+            let muteSocket = GetSocket(data.user, drawRooms[socket.index]['users']);
+
+            if(muteSocket !== null){
+                muteSocket.isMuted = !muteSocket.isMuted;
+                muteSocket.emit("muted");
+            }
+        }
     });
 
     //On user disconnection
-    socket.on('disconnect', () => {
-        let currentRoomIndex = roomSearch(socket.room, drawRooms);
+    socket.on("disconnect", () => {
+        let currentRoomIndex = RoomSearch(socket.room, drawRooms);
+
         //Check if room actually exists
         if(currentRoomIndex != null){
+
             //Delete user's socket from the room's socket array
             let socketIndex = drawRooms[currentRoomIndex]['users'].indexOf(socket);
             drawRooms[currentRoomIndex]['users'].splice(socketIndex, 1);
+
             //If room doesn't have any other user, delete the room
             if(drawRooms[currentRoomIndex]['users'].length === 0){
                 console.log("Room has no users, proceeding to delete");
                 drawRooms.splice(currentRoomIndex, 1);
-                console.log(drawRooms);
+            }else{
+                let data = {
+                    user: socket.username
+                }
+                socket.to(socket.room).emit("usrLeft", data);
             }
         }
     });
 
     socket.on('timeOut', () =>{
+
         //Check if room actually exists
         if(drawRooms[socket.index] !== undefined){
             console.log("Room has hit timeOut, proceeding to delete");
--- scripts/app3.js
+++ scripts/app3.js
@@ -1,17 +1,19 @@
 let App = {};
 document.addEventListener('DOMContentLoaded', function(){
     App.whiteboard = function(){
-        //const socket = io.connect('http://localhost:3000/')
-        const socket = io.connect('https://discord-notes.herokuapp.com/');
+        const socket = io.connect('http://localhost:3000/')
+        //const socket = io.connect('https://discord-notes.herokuapp.com/');
         const roomCode = document.querySelector('#roomCode').innerHTML;
+        let hostCode;
+        if(document.querySelector('#hostCode')){
+            hostCode = document.querySelector('#hostCode').innerHTML;
+        }
+        const errPar = document.querySelector('#error-paragraph');
+        const usrList = document.querySelector('.users-list');
+        const usrCont = document.querySelector('#user-clone-container');
     
-        //Tool elements
+        //Tool element
         const toolContainer = document.querySelector('#tools-container');
-        const blackMarker = document.querySelector('#blackMarker');
-        const greenMarker = document.querySelector('#greenMarker');
-        const redMarker = document.querySelector('#redMarker');
-        const blueMarker = document.querySelector('#blueMarker');
-        const eraser = document.querySelector('#eraser');
     
         //Timeout variables
         let timeOutID;
@@ -25,7 +27,13 @@ document.addEventListener('DOMContentLoaded', function(){
         let lastWorldCoord;
         let paths = [];
         let lastCoord;
-    
+
+        //User variables
+        let isHost = false;
+        let isMuted = false;
+        const userPrefix = 'user-';
+        
+        //Client timeout
         function TimeOut(){
             socket.emit("timeOut");
         }
@@ -80,6 +88,59 @@ document.addEventListener('DOMContentLoaded', function(){
                 paths.push(canvas[i]);
             }
         }
+
+        //Kick a user
+        function Kick(usr){
+            let userName = usr.slice(userPrefix.length, usr.length);
+
+            let data = {
+                user: userName
+            };
+            socket.emit('usrKick', data);
+        }
+
+        //Mute a user
+        function Mute(usr){
+            let userName = usr.slice(userPrefix.length, usr.length);
+            
+            let data = {
+                user: userName
+            };
+            socket.emit('usrMute', data);
+        }
+
+        //Users info init
+        function InitUsers(data){
+            let currentUserCont = usrCont.cloneNode(true);
+            let hostUserCont = usrCont.cloneNode(true);
+            hostUserCont.classList.add('user-container');
+            currentUserCont.classList.add('user-container');
+
+            //Check if the user is a host
+            if(isHost === true){
+                let kickBtn = document.createElement('button');
+                let muteBtn = document.createElement('button');
+                kickBtn.innerHTML = "Kick";
+                muteBtn.innerHTML = "Mute";
+                kickBtn.id = 'kick-btn';
+                muteBtn.id = 'mute-btn';
+                currentUserCont.appendChild(kickBtn);
+                currentUserCont.appendChild(muteBtn);
+            }
+
+            for(let i = 0; i < data.users.length; i++){
+                if(data.users[i].isHost){
+                    hostUserCont.querySelector('#userName').innerHTML = data.users[i].name + " (host)";
+                    hostUserCont.id = data.users[i].name;
+                    usrList.appendChild(hostUserCont);
+                }else{
+                    currentUserCont.querySelector('#userName').innerHTML = data.users[i].name;
+                    currentUserCont.id = userPrefix + data.users[i].name;
+                    usrList.appendChild(currentUserCont);
+                }
+            }
+
+        }
     
         //Canvas drag
         function Drag(canvas, ctx){
@@ -95,20 +156,59 @@ document.addEventListener('DOMContentLoaded', function(){
                 ctx.closePath();
             }
         }
-    
+        
+        //Drag function
         function ResizeCanvas(canvas, ctx){
             canvas.width = document.body.clientWidth;
             Drag(canvas, canvas.getContext('2d'));
         }
-    
-        //Join the desired room
-        socket.emit("joinRoom", roomCode);
+
+        //Join form
+        let joinForm = document.forms["joinForm"];
+
+        //If user created the room
+        if(hostCode){
+            joinForm.elements.host.value = hostCode;
+            $('#hostcheck').prop("checked", true);
+            $('#hostID').prop("disabled", false);
+        }
+
+        //On check box click
+        $("#hostcheck").click(function(event){
+            if($('#hostID').prop("disabled") === false){
+                $('#hostID').prop("disabled", true);
+            }else{
+                $('#hostID').prop("disabled", false);
+            }
+        });
+
+        //On submitting the join form
+        joinForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            let data;
+
+            //Join the desired room
+            if($('#hostID').prop("disabled") === true || $("#hostID").value === ""){
+                data = {
+                    room: roomCode,
+                    user: joinForm.elements.user.value,
+                    host: undefined
+                };
+            }else{
+                data = {
+                    room: roomCode,
+                    user: joinForm.elements.user.value,
+                    host: joinForm.elements.host.value
+                };
+            }
+            socket.emit("joinRoom", data);
+        });
     
         //Failure joining a room
-        socket.on("err", (err) => document.querySelector('#id-Container').innerHTML = err);
+        socket.on("err", (err) => errPar.innerHTML = err);
     
         //On successfully joining a room
-        socket.on("suc", (sentCanvas) => {
+        socket.on("suc", (data) => {
             let drawColor = "rgb(0,0,0)";
             let drawSize = 10;
     
@@ -118,9 +218,13 @@ document.addEventListener('DOMContentLoaded', function(){
             canvas.height = 900;
     
             //Draw the existing canvas
-            if(sentCanvas.length != 0){
-                InitCanvas(sentCanvas, ctx);
+            if(data.canvas.length != 0){
+                InitCanvas(data.canvas, ctx);
             }
+            
+            isHost = data.isHost;
+            console.log(isHost);
+            InitUsers(data);
     
             //Listening to other user draws/paths
             socket.on("draw", (data) => {
@@ -134,36 +238,88 @@ document.addEventListener('DOMContentLoaded', function(){
                 ctx.stroke();
                 ctx.closePath();
             });
-    
-            canvas.addEventListener('mousedown', function(event){
-                //Set first coord
-                if(event.buttons === 1){
-                    lastCoord = {x: event.clientX, y: event.clientY};
-                }else if(event.buttons === 2){
-                    lastWorldCoord = {x: event.clientX - rect.left, y: event.clientY - rect.top};
+
+            //Listen to when a new user joins the room
+            socket.on("usrJoin", (data) =>{
+                let currentUserCont = usrCont.cloneNode(true);
+
+                if(data.isHost){
+                    currentUserCont.querySelector('#userName').innerHTML = data.user + " (host)";
+                }else if(isHost){
+                    let kickBtn = document.createElement('button');
+                    let muteBtn = document.createElement('button');
+                    kickBtn.innerHTML = "Kick";
+                    muteBtn.innerHTML = "Mute";
+                    kickBtn.id = 'kick-btn';
+                    muteBtn.id = 'mute-btn';
+                    currentUserCont.appendChild(kickBtn);
+                    currentUserCont.appendChild(muteBtn);
+                    currentUserCont.querySelector('#userName').innerHTML = data.user;
+                }else{
+                    currentUserCont.querySelector('#userName').innerHTML = data.user;
+                }
+                currentUserCont.id = userPrefix + data.user;
+                currentUserCont.classList.add('user-container');
+                usrList.appendChild(currentUserCont);
+            });
+
+            socket.on("usrLeft", (data) =>{
+                let userCont = usrList.querySelector('#' + userPrefix + data.user);
+                if(userCont){
+                    usrList.removeChild(userCont);
                 }
             });
+
+            socket.on("kicked", () =>{
+                socket.emit("disconnect");
+                window.location.href = '/';
+            });
+
+            socket.on("muted", () =>{
+                isMuted = !isMuted;
+            });
+    
+            canvas.addEventListener('mousedown', function(event){
+                if(!isMuted){
+                    if(event.buttons === 1){
+
+                        //Set the first drawing coord
+                        lastCoord = {x: event.clientX, y: event.clientY};
+                    }else if(event.buttons === 2){
+    
+                        //Set the last world coord
+                        lastWorldCoord = {x: event.clientX - rect.left, y: event.clientY - rect.top};
+                    }
+                }
+            });
+
             //Erase
             canvas.addEventListener('contextmenu', function(event){
                 event.preventDefault();
             });
-            //Draw/Erase while moving mouse
+
+            //Draw or drag while moving mouse
             canvas.addEventListener('mousemove', function(event){
-                if(event.buttons === 1){
-                    Draw(canvas, event, drawColor, drawSize);
-                }else if(event.buttons === 2){
-                    worldOrigin.x = worldOrigin.x + (event.clientX - lastWorldCoord.x);
-                    worldOrigin.y = worldOrigin.y + (event.clientY - lastWorldCoord.y);
-                    console.log(worldOrigin);
-                    Drag(canvas, ctx);
-                    lastWorldCoord = {x: event.clientX - rect.left, y: event.clientY - rect.top};
+                if(!isMuted){
+                    if(event.buttons === 1){
+                        Draw(canvas, event, drawColor, drawSize);
+                    }else if(event.buttons === 2){
+                        document.body.style.cursor = "grab";
+                        worldOrigin.x = worldOrigin.x + (event.clientX - lastWorldCoord.x);
+                        worldOrigin.y = worldOrigin.y + (event.clientY - lastWorldCoord.y);
+                        Drag(canvas, ctx);
+                        lastWorldCoord = {x: event.clientX - rect.left, y: event.clientY - rect.top};
+                    }
                 }
             });
     
             canvas.addEventListener('mouseup', function(event){
-                if(event.buttons === 1){
-                    Draw(canvas, event, drawColor, drawSize);
+                if(!isMuted){
+                    if(event.buttons === 1){
+                        Draw(canvas, event, drawColor, drawSize);
+                    }
                 }
+                document.body.style.cursor = "default";
             });
     
             window.addEventListener('resize', function(){
@@ -173,11 +329,15 @@ document.addEventListener('DOMContentLoaded', function(){
             toolContainer.addEventListener('mousedown', function(event){
                 event.stopPropagation();
                 console.log(event.target);
+
+                //Check if hit a non-active tool
                 if(event.target.className !== "active-btn" && event.target.tagName.toLowerCase() === 'button'){
                     let currentTool = document.querySelector(".active-btn");
                     currentTool.classList.remove("active-btn");
                     event.target.classList.add("active-btn");
                     drawColor = event.target.value;
+
+                    //If eraser tool
                     if(event.target.value === 'white'){
                         drawSize = 40;
                     }else{
@@ -185,10 +345,28 @@ document.addEventListener('DOMContentLoaded', function(){
                     }
                 }
             });
+
+            usrList.addEventListener('mousedown', function(event){
+                if(event.target.id === 'kick-btn'){
+                    Kick(event.target.parentNode.id);
+                }
+                
+                if(event.target.id === 'mute-btn'){
+                    Mute(event.target.parentNode.id);
+
+                    if(event.target.classList.contains("active-btn")){
+                        event.target.classList.remove("active-btn");
+                    }else{
+                        event.target.classList.add("active-btn");
+                    }
+                }
+            });
     
     
             timeOutID = setInterval(TimeOut, timeOutLimit);
             document.body.querySelector('main').appendChild(canvas);
+            document.body.querySelector('.menus-container').style.display = "block";
+            joinForm.parentNode.removeChild(joinForm);
             ResizeCanvas(canvas);
         });
     }();
