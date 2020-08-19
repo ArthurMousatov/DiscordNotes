d50e432160543942b5936707cfe2e96ec1cf125a
Venoshulk <arthurmousatov@gmail.com>
2020-08-18 23:52:52 -0400

    Updated bot functionality

--- discordBot/bot.py
+++ discordBot/bot.py
@@ -12,46 +12,37 @@ bot = commands.Bot(command_prefix='?', case_insensitive=True)
 
 @bot.event
 async def on_ready():
-    print('Bot is ready')
+    print('Bot has been deployed')
 
 @bot.command()
 async def board(ctx, *args):
-    isMuted = "false"
 
-    if "mute" in args:
-        isMuted = "true"
-        print(isMuted)
-
-    response = RequestRoom(isMuted)
+    botMessage = await ctx.send("Making a room-creation request to the API. May take a couple of seconds!")
+    response = RequestRoom("true") if "mute" in args else RequestRoom("false")
+    await botMessage.delete(delay=1)
 
     if(response):
 
         if(response['status'] == "OK"):
             boardUrl = "%s/board/%s" % (baseUrl, response['roomCode'])
 
-            if "channel" in args and ctx.author.voice.channel:
+            if "text" in args:
+                await ctx.send("A board has been created! Please follow this link: " + boardUrl)
+            elif "voice" in args and ctx.author.voice.channel:
                 for member in ctx.author.voice.channel.members:
-                    await member.send("A board has been created! Please follow this link: " + boardUrl)
+                    if member != ctx.author:
+                        await member.send("A board has been created! Please follow this link: " + boardUrl)
+            elif "private" in args:
+                #Do nothing lole
+                print("Do nothing lole")
             else:
+                for arg in args:
+                    member = ctx.guild.get_member(GetMemberByMention(arg))
 
-                if len(args) > 0:
-
-                    for arg in args:
-                        #print(arg[3:-1])
-                        member = None
-
-                        try:
-                            member = ctx.guild.get_member(int(arg[3:-1]))
-                            break
-                        except:
-                            await ctx.send("Error occured during board sharing. Make sure the @mentions are correct!")
-
-                        if member and not member.bot:
-                            print("Sending board to: %s" % member.nick)
-                            await member.send("A board has been created! Please follow this link: " + boardUrl)
-                else:
-                    await ctx.send("A board has been created! Please follow this link: " + boardUrl)
-            
+                    if member and not member.bot:
+                        print("Sending board to: %s" % member.nick)
+                        await member.send("A board has been created! Please follow this link: " + boardUrl)
+                        
             await ctx.author.send("A board has been created! Please follow this link: %s\n Here is the host ID to give you muting and kicking privileges: %s" % (boardUrl, str(response['hostCode'])))
         else:
             await ctx.send("Couldn't create a board. Status code: %s" % response['status'])
@@ -67,6 +58,14 @@ def RequestRoom(isMuted):
         r = requests.get(url = reqUrl)
         return r.json()
     except:
+        return None
+
+def GetMemberByMention(mention: str):
+    if mention.startswith("<@!") and mention.endswith(">") and mention[3:-1].isdigit():
+        return int(mention[3:-1])
+    elif mention.startswith("<@") and mention.endswith(">") and mention[2:-1].isdigit():
+        return int(mention[2:-1])
+    else:
         return None
 
 
